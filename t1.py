fae561492ac80eebdfbea81c95604d9c5b61ae5c
ShubhamPataskar <168623279+ShubhamPataskar@users.noreply.github.com>
2024-05-01 13:08:23 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,1 +1,4 @@
 print("hello world")
+print("Google App Engine")
+Print("This python document is deployed with the help of google cloud platform which is cloned via github repository.")
+print("Thank you !")
